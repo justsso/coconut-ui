339646a4ab6d5b778fa80a3181fd0a4de596a260
sso just <528226249@qq.com>
2019-12-31 23:56:34 +0800

    add layout

---
+++ components/layout/index.tsx
@@ -0,0 +1,10 @@
+import Layout from './layout';
+import Sider from './Sider';
+
+export {BasicProps as LayoutProps} from './layout';
+export {SiderProps} from './Sider';
+
+
+Layout.Sider = Sider;
+
+export default Layout;
