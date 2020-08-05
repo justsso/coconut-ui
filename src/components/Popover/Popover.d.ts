ee61ba7bb864e280ced52c7e535e14283c124245
justsso <528226249@qq.com>
2020-08-05 23:56:03 +0800

    feat: 增加Popover组件

---
+++ src/components/Popover/Popover.d.ts
@@ -0,0 +1,10 @@
+/**
+ * @description
+ * @author justsso
+ */
+
+interface PopoverProps {
+    title: string,
+    trigger: 'hover' | 'foucs' | 'click',
+    position: 'top' | 'bottom' | 'right' | 'left',
+}
