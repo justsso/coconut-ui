fb7768386234b0a07b2075a8e6a4d8d735ac895e
justsso <528226249@qq.com>
2020-09-06 20:22:16 +0800

    popover 组件

---
+++ demos/popover/data.js
@@ -0,0 +1,12 @@
+/**
+ * @description
+ * @author justsso
+ */
+const popoverPropsData = [
+    ["参数", "说明", "类型", "可选值", "默认值"],
+    ["title", "展示的标题", "string| React.Element", "--", "--"],
+    ['content', "展示的内容", "string| React.Element", "--", "--"],
+    ["trigger", "触发方式", "string", "hover | focus | click", "hover"],
+    ["position", "要展示的位置", "string", "top | bottom | left | right", "right"]
+]
+export default popoverPropsData;
